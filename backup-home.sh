e267f7bc63452dc1e607e6c13518efd383edb6d5
DaviVilarinho <davi.r.vilarinho@gmail.com>
2023-12-05 23:50:54 -0300

    can backup home

--- backup-home.sh
+++ backup-home.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+set -e
+
 code --list-extensions >> ~/.config/vscode-extensions.log
 
 DRIVE_SYNC_DIR="$HOME/mnt/drive-sync"
@@ -21,18 +23,10 @@ then
   rclone sync ~/docs hdd:/docs -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" && notify-send "sync docs to hdd"
 fi
 
-rclone sync ~/.config drive-encrypted:/notebook/config -v --exclude="**/.git/**" --exclude="Code/**" --exclude --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" --exclude="**/Teams/**" --exclude="Microsoft/**" --exclude="Microsoft Teams - Preview/**" && notify-send "sync .config to drive encrypted"
-
-if [[ $is_mountpoint == 0 ]]
-then
-  rclone sync ~/.config hdd:/config -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" --exclude="Microsoft/**" --exclude="Microsoft Teams - Preview/**" && notify-send "sync doconfig to hdd"
-fi
-
-
 for d in $HOME/*/
 do
-  based = $(basename $d)
-  if [[ based != "mnt" && based != "go" && based != "docs" ]]
+  based=$(basename $d)
+  if [[ $based != "mnt" && $based != "go" && $based != "docs" ]]
   then
     rclone sync $d drive-encrypted:/notebook/$based -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" && notify-send "sync $based to drive encrypted"
 
@@ -42,6 +36,14 @@ do
     fi
   fi
 done
+
+rclone sync ~/.config drive-encrypted:/notebook/config -v --exclude="**/.git/**" --exclude="Code/**" --exclude --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" --exclude="**/Teams/**" --exclude="Microsoft/**" --exclude="Microsoft Teams - Preview/**" && notify-send "sync .config to drive encrypted"
+
+if [[ $is_mountpoint == 0 ]]
+then
+  rclone sync ~/.config hdd:/config -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" --exclude="Microsoft/**" --exclude="Microsoft Teams - Preview/**" && notify-send "sync doconfig to hdd"
+fi
+
 
 
 rclone sync drive-encrypted:/ ufudrive-encrypted:/ --progress
