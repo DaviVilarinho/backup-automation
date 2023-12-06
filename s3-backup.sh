73763aac05738b6500551217fcaae28f71f64f30
DaviVilarinho <davi.r.vilarinho@gmail.com>
2023-12-06 08:53:58 -0300

    s3 copy

--- s3-backup.sh
+++ s3-backup.sh
@@ -1,12 +1,15 @@
+#!/bin/zsh
 HDD_DIR="$HOME/mnt/hdd"
+
+set -e
 
 mountpoint $HDD_DIR
 is_mountpoint=$?
 
 if [ $is_mountpoint -eq 0 ]; then
-    rclone sync --fast-list --checksum hdd:/docs s3_archive:/docs --no-traverse
+    rclone sync --fast-list --checksum hdd:/docs s3_archive:/docs --no-traverse --progress
 
-    rclone sync --fast-list --checksum hdd:/archive s3_archive_encrypted: --no-traverse
+    rclone sync --fast-list --checksum hdd:/archive s3_archive_encrypted: --no-traverse --progress
 else
     echo "HDD is not mounted"
     exit 1
