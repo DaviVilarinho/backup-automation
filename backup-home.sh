136ec4a18b45f08049d60adb12eca1f864a4e979
DaviVilarinho <davi.r.vilarinho@gmail.com>
2024-02-03 15:20:56 -0300

    changes in structure

--- backup-home.sh
+++ backup-home.sh
@@ -2,48 +2,19 @@
 
 code --list-extensions >> ~/.config/vscode-extensions.log
 
-DRIVE_SYNC_DIR="$HOME/mnt/drive-sync"
+DRIVE_SYNC_DIR="$HOME/mnt/drive"
 HDD_DIR="$HOME/mnt/hdd"
 
-rclone sync drive:/sync $DRIVE_SYNC_DIR/sync -v && notify-send "sync sync dir from drive"
-rclone sync drive:/colabTempResults $DRIVE_SYNC_DIR/colabTempResults -v && notify-send "sync colabTempResults dir from drive"
-rclone sync drive:/Colab\ Notebooks $DRIVE_SYNC_DIR/Colab\ Notebooks -v && notify-send "sync Colab Notebooks dir from drive"
-
-rclone sync ~/docs drive-encrypted:/docs -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" && notify-send "sync docs to drive encrypted"
-
-rclone sync ~/docs drive:/docs -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" && notify-send "sync docs to drive"
+(rclone sync drive:/sync $DRIVE_SYNC_DIR/sync -v && notify-send "sync sync dir from drive") &
+(rclone sync drive:/Colab\ Notebooks $DRIVE_SYNC_DIR/Colab\ Notebooks -v && notify-send "sync Colab Notebooks dir from drive") &
+(rclone sync ~/data/docs drive:/docs -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" && notify-send "sync docs to drive encrypted") &
+(rclone sync ~/data/notes drive:/notes -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" && notify-send "sync notes to drive encrypted") &
 
 if [[ $(mountpoint $HDD_DIR) == 0 ]]
 then
-  rclone sync ~/docs hdd:/docs -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" && notify-send "sync docs to hdd"
+  (rclone sync ~/data hdd:/data -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" && notify-send "sync docs to hdd") & 
+  rclone sync ~/backups hdd:/backups -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" && notify-send "sync docs to hdd"
 fi
 
-for d in $HOME/*/
-do
-  based=$(basename $d)
-  if [[ $based != "mnt" && $based != "go" && $based != "docs" ]]
-  then
-    rclone sync $d drive-encrypted:/notebook/$based -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" && notify-send "sync $based to drive encrypted"
-
-    if [[ $(mountpoint $HDD_DIR) == 0 ]]
-    then
-      rclone sync $d hdd:/home/$based -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" && notify-send "sync $based to hdd"
-    fi
-  fi
-done
-
-rclone sync ~/.config drive-encrypted:/notebook/config -v --exclude="**/.git/**" --exclude="Code/**" --exclude --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" --exclude="**/Teams/**" --exclude="Microsoft/**" --exclude="Microsoft Teams - Preview/**" && notify-send "sync .config to drive encrypted"
-
-if [[ $(mountpoint $HDD_DIR) == 0 ]]
-then
-  rclone sync ~/.config hdd:/config -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" --exclude="Microsoft/**" --exclude="Microsoft Teams - Preview/**" && notify-send "sync doconfig to hdd"
-fi
-
-
-
-rclone sync drive-encrypted:/ ufudrive-encrypted:/ --progress
-
-if [[ $(mountpoint $HDD_DIR) == 0 ]]
-then
-  rclone sync hdd:/archive drive-encrypted:/archive -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" && notify-send "sync hdd to sync"
-fi
+(rclone sync ~/data drive-encrypted:/data -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" && notify-send "sync docs to drive") &
+rclone sync ~/data ufudrive-encrypted:/ --progress  && notify-send 'sync to ufudrive encrypted'
--- s3-backup.sh
+++ s3-backup.sh
@@ -7,9 +7,9 @@ mountpoint $HDD_DIR
 is_mountpoint=$?
 
 if [ $is_mountpoint -eq 0 ]; then
-    rclone sync --fast-list --checksum hdd:/docs s3_archive:/docs --no-traverse --progress
+    rclone sync --fast-list --checksum ~/data/docs s3_archive:/docs --no-traverse --progress
 
-    rclone sync --fast-list --checksum hdd:/archive s3_archive_encrypted: --no-traverse --progress
+    rclone sync --fast-list --checksum ~/data s3_archive_encrypted: --no-traverse --progress
 else
     echo "HDD is not mounted"
     exit 1
