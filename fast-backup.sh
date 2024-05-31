ac2a2c736219fb70a3db670174d8e832d8bdb269
DaviVilarinho <davi.r.vilarinho@gmail.com>
2024-05-31 16:58:09 -0300

    fast backup

--- fast-backup.sh
+++ fast-backup.sh
@@ -12,14 +12,14 @@ fi
 
 sleep 10
 
-(rclone sync drive:/sync $DRIVE_SYNC_DIR/sync -v && notify-send "sync sync dir from drive") &
-(rclone sync drive:/Colab\ Notebooks $DRIVE_SYNC_DIR/Colab\ Notebooks -v && notify-send "sync Colab Notebooks dir from drive") &
-(rclone sync ~/data/docs drive:/docs -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" && notify-send "sync docs to drive") &
-(rclone sync ~/data/notes drive:/notes -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" && notify-send "sync notes to drive") &
-(rclone sync ~/data/projects drive:/desktop-projects -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" && notify-send "sync projects to drive") &
+(rclone sync drive:/sync $DRIVE_SYNC_DIR/sync -v --tpslimit=6 --checkers=20 --drive-chunk-size=32M && notify-send "sync sync dir from drive") &
+(rclone sync drive:/Colab\ Notebooks $DRIVE_SYNC_DIR/Colab\ Notebooks -v && notify-send "sync Colab Notebooks dir from drive" --tpslimit=6 --checkers=20 --drive-chunk-size=32M) &
+(rclone sync ~/data/docs drive:/docs -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" --tpslimit=6 --checkers=20 --drive-chunk-size=32M && notify-send "sync docs to drive") &
+(rclone sync ~/data/notes drive:/notes -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" --tpslimit=6 --checkers=20 --drive-chunk-size=32M && notify-send "sync notes to drive") &
+(rclone sync ~/data/projects drive:/desktop-projects -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" --tpslimit=6 --checkers=20 --drive-chunk-size=32M && notify-send "sync projects to drive") &
 
 if [[ $(mountpoint $HDD_DIR) == 0 ]]
 then
-  (rclone sync ~/data hdd:/data -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" && notify-send "sync data to hdd") & 
-  rclone sync ~/backups hdd:/backups -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" && notify-send "sync backus to hdd"
+  (rclone sync ~/data hdd:/data -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" --tpslimit=6 --checkers=20 --drive-chunk-size=32M && notify-send "sync data to hdd") & 
+  rclone sync ~/backups hdd:/backups -v --exclude="**/.git/**" --exclude="**/node_modules/**" --exclude="**/.env/**" --exclude="**/.venv/**" --exclude="**/.cache/**" --exclude="**/.Cache/**" --tpslimit=6 --checkers=20 --drive-chunk-size=32M && notify-send "sync backus to hdd"
 fi
